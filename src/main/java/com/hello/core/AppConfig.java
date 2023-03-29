92072dcc4475452dd6489479ba9ad2b5afdc9196
bw1611 <bw1611@naver.com>
2023-03-29 22:22:56 +0900

    Bean을 통해 SpringContainer에 저장

--- src/main/java/com/hello/core/AppConfig.java
+++ src/main/java/com/hello/core/AppConfig.java
@@ -2,29 +2,36 @@ package com.hello.core;
 
 import com.hello.core.discount.DiscountPolicy;
 import com.hello.core.discount.FixDiscountPolicy;
+import com.hello.core.discount.RateDiscountPolicy;
 import com.hello.core.member.MemberRepository;
 import com.hello.core.member.MemberService;
 import com.hello.core.member.MemberServiceImpl;
 import com.hello.core.member.MemoryMemberRepository;
 import com.hello.core.order.OrderService;
 import com.hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 설정 정보에 @Configuration 적어준다.
 public class AppConfig {
-
-    // 생성자 주입
+    
+    @Bean //    @Bean("mmm") // bean의 이름도 바꿀 수있지만 관례상 안바꾸는게 좋다.
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(new MemoryMemberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+//        return new FixDiscountPolicy();
     }
 }
--- src/main/java/com/hello/core/MemberApp.java
+++ src/main/java/com/hello/core/MemberApp.java
@@ -4,11 +4,17 @@ import com.hello.core.member.Grade;
 import com.hello.core.member.Member;
 import com.hello.core.member.MemberService;
 import com.hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); // AppConfig에 있는 정보를 applicationContext에 넣어준다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);// 기본적으로 메서드 이름으로 등록되기 떄문에 memberService 를 선언해주었다.
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
